f0660bfa64eebde6cb3f7626bffaab270acf03d1
ksturgeong <ksturgeongcc@gmail.com>
2022-09-16 13:13:40 +0100

    made first change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          source code has been edited an pushed to netlify
         </p>
         <a
           className="App-link"
